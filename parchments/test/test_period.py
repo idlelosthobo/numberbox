8499368a62cfbb2f5c3299751f592d70e5844545
Nathan Johnson <nathan@tegroup.ca>
2021-03-01 09:19:38 -0700

    changed pervious and next period to properties.

--- parchments/test/test_period.py
+++ parchments/test/test_period.py
@@ -41,60 +41,60 @@ class TestPeriod(unittest.TestCase):
     def test_previous_period_year_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='year')
         period_test_grid.add_period(datetime.datetime(2020, 4, 1), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].previous_period().key == '20190101')
+        self.assertTrue(period_test_grid.column_index[0].previous_period.key == '20190101')
 
     def test_next_period_year_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='year')
         period_test_grid.add_period(datetime.datetime(2020, 4, 1), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].next_period().key == '20210101')
+        self.assertTrue(period_test_grid.column_index[0].next_period.key == '20210101')
 
     def test_previous_period_month_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='month')
         period_test_grid.add_period(datetime.datetime(2020, 4, 1), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].previous_period().key == '20200301')
+        self.assertTrue(period_test_grid.column_index[0].previous_period.key == '20200301')
 
     def test_previous_period_year_roll_over_month_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='month')
         period_test_grid.add_period(datetime.datetime(2020, 1, 1), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].previous_period().key == '20191201')
+        self.assertTrue(period_test_grid.column_index[0].previous_period.key == '20191201')
 
     def test_next_period_month_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='month')
         period_test_grid.add_period(datetime.datetime(2020, 4, 1), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].next_period().key == '20200501')
+        self.assertTrue(period_test_grid.column_index[0].next_period.key == '20200501')
 
     def test_next_period_year_roll_over_month_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='month')
         period_test_grid.add_period(datetime.datetime(2020, 12, 1), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].next_period().key == '20210101')
+        self.assertTrue(period_test_grid.column_index[0].next_period.key == '20210101')
 
     def test_previous_period_day_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='day')
         period_test_grid.add_period(datetime.datetime(2020, 4, 10), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].previous_period().key == '20200409')
+        self.assertTrue(period_test_grid.column_index[0].previous_period.key == '20200409')
 
     def test_previous_period_month_roll_over_day_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='day')
         period_test_grid.add_period(datetime.datetime(2020, 4, 1), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].previous_period().key == '20200331')
+        self.assertTrue(period_test_grid.column_index[0].previous_period.key == '20200331')
 
     def test_previous_period_year_roll_over_day_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='day')
         period_test_grid.add_period(datetime.datetime(2020, 1, 1), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].previous_period().key == '20191231')
+        self.assertTrue(period_test_grid.column_index[0].previous_period.key == '20191231')
 
     def test_next_period_day_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='day')
         period_test_grid.add_period(datetime.datetime(2020, 4, 10), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].next_period().key == '20200411')
+        self.assertTrue(period_test_grid.column_index[0].next_period.key == '20200411')
 
     def test_next_period_month_roll_over_day_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='day')
         period_test_grid.add_period(datetime.datetime(2020, 4, 30), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].next_period().key == '20200501')
+        self.assertTrue(period_test_grid.column_index[0].next_period.key == '20200501')
 
     def test_next_period_year_roll_over_day_iteration(self):
         period_test_grid = parchments.Grid(TEST_INDEX, period_iteration='day')
         period_test_grid.add_period(datetime.datetime(2020, 12, 31), PERIOD_DATA)
-        self.assertTrue(period_test_grid.column_index[0].next_period().key == '20210101')
+        self.assertTrue(period_test_grid.column_index[0].next_period.key == '20210101')
 
