4daa02f99611c15f4e3c669fd7f9a367949c2e0a
Nathan <drugen@gmail.com>
2021-03-16 22:15:21 -0600

    debug update

--- parchments/core/debug.py
+++ parchments/core/debug.py
@@ -1,11 +1,12 @@
 import time
 
 
-def execution_time(method):
+def execution_time(method, repeat_count=1):
     def timed(*args, **kwargs):
-        ts = time.time()
-        result = method(*args, **kwargs)
-        te = time.time()
-        print('%r  %2.2f ms' % (method.__name__, (te - ts) * 1000))
+        for i in range(repeat_count):
+            ts = time.time()
+            result = method(*args, **kwargs)
+            te = time.time()
+            print('>>> function %r executed in %2.2f ms <<<' % (method.__name__, (te - ts) * 1000))
 
     return timed
